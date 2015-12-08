f90b5fe7df18cbd8b19d05c703ee626a52b79e06
nicolaez <nicolae.klf@gmail.com>
2015-12-08 16:06:46 -0500

    employee

---
+++ application/models/Employee.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: nicol
+ * Date: 12/8/2015
+ * Time: 3:54 PM
+ */
+class Employee extends CI_Model
+{
+    private $id_emp;
+    private $name;
+    private $email;
+  private $position;
+  private $avatar_blob;
+  private $avatar_url;
+  private $hire_date;
+}
