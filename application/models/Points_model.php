dfdf57c062c13842a347252a5a1cdbe476de6842
Vitalie SUBA <vitalie.suba@gmail.com>
2015-12-08 18:01:17 -0500

    separated files

--- application/models/Points_model.php
+++ application/models/Points_model.php
@@ -5,3 +5,9 @@
  * Date: 08/12/2015
  * Time: 4:02 PM
  */
+
+class Points_model extends CI_Model
+{
+
+
+}
---
+++ application/views/header.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="description" content="">
+  <meta name="author" content="">
+
+  <title>SB Admin - Bootstrap Admin Template</title>
+
+  <!-- Bootstrap Core CSS -->
+  <link href="<?php echo base_url(); ?>assets/css/bootstrap.min.css" rel="stylesheet">
+
+  <!-- Custom CSS -->
+  <link href="<?php echo base_url(); ?>assets/css/sb-admin.css" rel="stylesheet">
+
+  <!-- Morris Charts CSS -->
+  <link href="<?php echo base_url(); ?>assets/css/plugins/morris.css" rel="stylesheet">
+
+  <!-- Custom Fonts -->
+  <link href="<?php echo base_url(); ?>font-awesome/assets/css/font-awesome.min.css" rel="stylesheet"
+        type="text/css">
+
+
+</head>
+
+<body>
