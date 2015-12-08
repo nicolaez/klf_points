67dd6eaa94382bef83a0b37f6bd562490d1cd0c8
nicolaez <nicolae.klf@gmail.com>
2015-12-08 16:02:54 -0500

    db access

--- application/config/database.php
+++ application/config/database.php
@@ -75,10 +75,10 @@ $query_builder = TRUE;
 
 $db['default'] = array(
 	'dsn'	=> '',
-	'hostname' => 'localhost',
-	'username' => '',
-	'password' => '',
-	'database' => '',
+	'hostname' => '192.168.1.98',
+	'username' => 'root',
+	'password' => '123456',
+	'database' => 'klfpoints',
 	'dbdriver' => 'mysqli',
 	'dbprefix' => '',
 	'pconnect' => FALSE,
