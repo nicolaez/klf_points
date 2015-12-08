a95c5f354d51e643f7febe9ad178f0c63c7c3ae7
nicolaez <nicolae.klf@gmail.com>
2015-12-08 16:19:57 -0500

    employee

--- application/models/Employee.php
+++ application/models/Employee.php
@@ -7,11 +7,26 @@
  */
 class Employee extends CI_Model
 {
-    private $id_emp;
-    private $name;
-    private $email;
+  private $id_emp;
+  private $name;
+  private $email;
   private $position;
   private $avatar_blob;
   private $avatar_url;
   private $hire_date;
+  private $points;
+  private $emp_type;
+  private $passwd;
+  private $active;
+
+  public function __construct()
+  {
+    parent::__construct();
+  }
+
+  public function add($data)
+  {
+    $this->db->set($data);
+    $this->db->insert('employee', $data);
+  }
 }
