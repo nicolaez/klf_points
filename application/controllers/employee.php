0834533d4b5cab9f4d5b3709046cd377e9375eb3
nicolaez <nicolae.klf@gmail.com>
2015-12-08 17:23:58 -0500

    update

---
+++ application/controllers/employee.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: nicol
+ * Date: 12/8/2015
+ * Time: 4:10 PM
+ */
+defined('BASEPATH') OR exit('No direct script access allowed');
+class Employee extends CI_Controller
+{
+    public function __construct()
+    {
+      parent::__construct();
+      $this->load->model('Employee_model');
+      $this->load->helper('form');
+    }
+
+  public function index()
+  {
+      $this->load->view('index');
+  }
+
+  public function add_emp()
+  {
+    $this->load->model('Employee_model');
+      $data = array(
+          'name'=> 'Vasea Pupkin',
+          'email'=>'a@a.com',
+          'position'=>'boss',
+          'points'=>100,
+          'type'=>'admin',
+          'passwd'=>'123456'
+      );
+    $emp = new Employee_model();
+    $emp->add($data);
+  }
+}
