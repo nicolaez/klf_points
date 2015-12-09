393962799eec1473d14315e11a0ae6cc8deb2a6b
Vitalie SUBA <vitalie.suba@gmail.com>
2015-12-09 08:49:21 -0500

    Ading admin controller

---
+++ application/controllers/Admin.php
@@ -0,0 +1,20 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Admin extends CI_Controller {
+
+  public function __construct()
+  {
+    parent:: __construct();
+  }
+
+  public function index()
+  {
+    //	$this->load->helper('url');
+    $this->load->view('header');
+    $this->load->view('add_employee');
+    $this->load->view('footer');
+    //$this->load->view('default');
+
+  }
+}
