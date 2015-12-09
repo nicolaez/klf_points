435212bc1f771347f755c6484713693d45e9d705
Vitalie SUBA <vitalie.suba@gmail.com>
2015-12-09 11:20:38 -0500

    navbar

--- application/views/settings.php
+++ application/views/settings.php
@@ -1,5 +1,9 @@
 
-    <div id="wrapper">
+<?php
+require 'header.php';
+?>
+
+<div id="wrapper">
 
         <!-- Navigation -->
         <?php require 'nav.php'; ?>
@@ -12,7 +16,7 @@
                 <div class="row">
                     <div class="col-lg-12">
                         <h1 class="page-header">
-                            Add points
+                            Template
                         </h1>
                         <ol class="breadcrumb">
                             <li class="active">
@@ -24,25 +28,7 @@
                 <!-- /.row -->
 
                 <div class="row">
-                    <div class="col-md-5 col-md-offset-1">
-                        <?php
 
-                        echo form_open('Employee/add_emp');
-
-                        echo form_label('Add number of points: ');
-                        echo form_input(array('id'=>'points', 'name'=>'points', 'class'=>'form-control'),set_value ('points'),'autpfocus');
-                        echo '<div class="error_control">'.form_error('points').'</div>';
-
-                        echo form_label('Description: ');
-                        echo form_textarea(array('id'=>'description', 'name'=>'description', 'rows'=> 7, 'class'=>'form-control'), set_value('description'));
-                        echo '<div class="error_control">'.form_error('lname').'</div>';
-
-                        echo form_submit(array('id'=>'btnAdd', 'name'=>'btnAdd','class'=>'btn btn-primary'),'Submit');
-                        echo form_reset(array('id'=>'btnClr', 'name'=>'btnClr','class'=>'btn btn-primary'),'Reset');
-
-                        echo form_close();
-                        ?>
-                    </div>
                 </div>
                 <!-- /.row -->
 
@@ -72,3 +58,6 @@
     <!-- /#wrapper -->
 
 
+<?php
+require 'footer.php';
+?>
